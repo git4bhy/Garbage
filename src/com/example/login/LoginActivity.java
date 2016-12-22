9a7b79896bc6368f5f44b3f37124b74d48dbc43b
bhy <bhy_1989@sina.com>
2016-12-22 13:45:48 +0800

    111

--- src/com/example/login/LoginActivity.java
+++ src/com/example/login/LoginActivity.java
@@ -39,7 +39,7 @@ public class LoginActivity extends Activity {
 		username.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				username.setText("");
+				username.setText("111");
 			}
 		});
 	
